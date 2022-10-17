c2da232ee6acfd1fb7f2f9d8f18d554172b8f797
Mateus Camargo Rodrigues <mateus-mac@Mateuss-MacBook-Pro.local>
2022-10-16 22:46:14 -0300

    Removendo cors confid

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
   await app.listen(3000, () => console.log('Ambiente rodando na porta 3000'));
 }
 bootstrap();
